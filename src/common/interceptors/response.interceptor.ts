ef1d983108b2aaf74b5809e9d4fcb42fb483d197
eunkang-choi <dmsrkd1216@gmail.com>
2026-01-27 04:56:17 +0900

    chore: 정상일때 응답데이터 인터셉터 작성

---
+++ src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Response } from 'express';
+import { ApiResponse } from '../interfaces/response.interface';
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, ApiResponse<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<ApiResponse<T>> {
+    const response = context.switchToHttp().getResponse<Response>();
+
+    return next.handle().pipe(
+      map((data: T) => ({
+        statusCode: response.statusCode,
+        success: true,
+        data,
+      })),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   setupSwagger(app);
 
