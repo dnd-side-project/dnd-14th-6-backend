acabfb4c329cb19eef731f34add5ed0bd700c34b
eunkang-choi <dmsrkd1216@gmail.com>
2026-01-26 18:35:06 +0900

    chore: husky 깃컨벤션 적용 라이브러리 설치

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'fix', // 버그수정
+        'refactor', // 코드 리팩터링
+        'docs', // 리드미 작성
+        'chore', // 패키지 설치, 기능단위가 아닌 작업
+        'init', // 프로젝트 초기셋팅
+        'hotfix', // 이슈, QA에서 급한 버그수정
+        'remove', // 불필요 코드/파일 삭제
+        'rename', // 파일 이름 변경
+        'test', // 테스트파일 관련 작업
+      ],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+  },
+};
