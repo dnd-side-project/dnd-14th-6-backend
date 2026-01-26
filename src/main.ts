277ad24a30eef3604964251801648227ea6dd832
eunkang-choi <dmsrkd1216@gmail.com>
2026-01-27 02:23:05 +0900

    chore: /api prefix 추가

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { setupSwagger } from './config/swagger.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   setupSwagger(app);
 
   await app.listen(process.env.PORT ?? 3000);
