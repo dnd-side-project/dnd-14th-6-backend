60dc3728995a6639e3040760a8c70c527e9859d5
eunkang-choi <dmsrkd1216@gmail.com>
2026-01-29 19:27:57 +0900

    fix: commitlint.config.js 파일 eslint.config.mjs의 ignores에 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', '.husky/**'],
+    ignores: ['eslint.config.mjs', '.husky/**', 'commitlint.config.js'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
