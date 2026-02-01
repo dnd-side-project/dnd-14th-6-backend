a1ad321f7293f747630d3d397764549f3aa86f8a
jokj624 <jokj624@gmail.com>
2026-02-01 18:58:50 +0900

    feat: error exception filter 추가

---
+++ src/common/filters/error-exception.filter.ts
@@ -0,0 +1,47 @@
+import { Response, Request } from 'express';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+
+import { ResponseExceptionDto } from '@common/dto/response-exception.dto';
+
+@Catch()
+export class ErrorExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(ErrorExceptionFilter.name);
+
+  catch(exception: Error, host: ArgumentsHost) {
+    if (exception instanceof TypeError || exception instanceof HttpException) {
+      throw exception;
+    }
+
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+
+    let endpoint: string;
+    try {
+      console.log(`${request.method} ${request.url}`);
+      endpoint = decodeURIComponent(`${request.method} ${request.url}`);
+    } catch (error) {
+      this.logger.error(error);
+
+      endpoint = `${request.method} ${request.url}`;
+    }
+
+    const errorResponse = {
+      message: exception.message || 'Internal server error',
+      statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      endpoint,
+      stack: exception.stack,
+    };
+
+    this.logger.error(errorResponse);
+
+    response.status(HttpStatus.INTERNAL_SERVER_ERROR).json(ResponseExceptionDto.of(errorResponse));
+  }
+}
