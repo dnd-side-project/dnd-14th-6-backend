4a7708b8d81031e560df6699ac1b49476894448c
eunkang-choi <dmsrkd1216@gmail.com>
2026-02-05 03:57:42 +0900

    chore: games.module alias로 @/games ->  @games/...

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { GamesService } from '@/games/application/games.service';
-import { GamesController } from '@/games/presentation/games.controller';
+import { GamesService } from '@games/application/games.service';
+import { GamesController } from '@games/presentation/games.controller';
 import { GAME_REPOSITORY } from '@games/domain/games.repository.interface';
 import { GameRepository } from '@games/infra-structure/games.repository';
 import { PrismaModule } from '@prisma/prisma.module';
