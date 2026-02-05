55f7dff767808d729f5c40707625aee53e92728e
eunkang-choi <dmsrkd1216@gmail.com>
2026-02-06 02:20:58 +0900

    rename: 폴더디렉터리명 변경 infra-structure -> infrastructure

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { GamesService } from '@games/application/games.service';
 import { GamesController } from '@games/presentation/games.controller';
 import { GAME_REPOSITORY } from '@games/domain/games.repository.interface';
-import { GameRepositoryImpl } from '@games/infra-structure/games.repository';
+import { GameRepositoryImpl } from '@/games/infrastructure/games.repository';
 import { PrismaModule } from '@prisma/prisma.module';
 
 @Module({
