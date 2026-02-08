2da5c67aaaf7886119b849fa2b6dcc2f44fe7e0f
jokj624 <jokj624@gmail.com>
2026-02-08 19:00:08 +0900

    feat: user stats 조회용 repository 함수 interface 추가

--- src/users/domain/users.repository.interface.ts
+++ src/users/domain/users.repository.interface.ts
@@ -1,8 +1,18 @@
 import { User } from './users.entity';
 
+export interface ScoreDetailRaw {
+  difficultyMode: string;
+  category: string;
+  totalScore: number;
+}
+
 export interface IUsersRepository {
   findAllOrderByScoreDesc(page: number, size: number, tierId?: number): Promise<User[]>;
   countAll(tierId?: number): Promise<number>;
+  findByIdWithTier(userId: bigint): Promise<User | null>;
+  getAverageScore(): Promise<number>;
+  getRankingByScore(totalScore: bigint): Promise<number>;
+  getScoreDetailByUserId(userId: bigint): Promise<ScoreDetailRaw[]>;
 }
 
 export const USER_REPOSITORY = Symbol('IUsersRepository');
