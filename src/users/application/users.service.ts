9fcd55f74aa09762b842c12eeb8764f34dfe8936
jokj624 <jokj624@gmail.com>
2026-02-08 19:02:56 +0900

    feat: 유저 스탯 조회 서비스 함수

--- src/users/application/users.service.ts
+++ src/users/application/users.service.ts
@@ -1,7 +1,9 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 
 import { IUsersRepository, USER_REPOSITORY } from '../domain/users.repository.interface';
 import { User } from '../domain/users.entity';
+import { UserStats } from '../domain/user-stats.entity';
+import { UserStatsMapper } from '../domain/user-stats.mapper';
 
 @Injectable()
 export class UsersService {
@@ -16,5 +18,33 @@ export class UsersService {
       this.usersRepository.findAllOrderByScoreDesc(page, size, tierId),
       this.usersRepository.countAll(tierId),
     ]);
+  }
+
+  /**
+   * @description 유저의 랭킹, 티어, 총 점수, 카테고리 별 누적점수를 조회
+   */
+  async getUserStats(userId: bigint): Promise<UserStats> {
+    const [user, avgScore, scoreDetailRaw] = await Promise.all([
+      this.usersRepository.findByIdWithTier(userId),
+      this.usersRepository.getAverageScore(),
+      this.usersRepository.getScoreDetailByUserId(userId),
+    ]);
+
+    // FIXME: auth guard 추가 시 필요 없어짐 (guard에서 리소스 확인)
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+
+    const ranking = await this.usersRepository.getRankingByScore(user.totalScore);
+    const scoreDetail = UserStatsMapper.groupAndSortScoreDetail(scoreDetailRaw);
+
+    return UserStats.from({
+      nickname: user.nickname,
+      totalScore: user.totalScore,
+      averageScore: Math.round(avgScore),
+      ranking,
+      tier: user.tier,
+      scoreDetail,
+    });
   }
 }
