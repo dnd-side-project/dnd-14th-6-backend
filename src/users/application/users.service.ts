f7e53274ac40f9a8ed9cb301728548df255fb91d
jokj624 <jokj624@gmail.com>
2026-02-10 01:36:13 +0900

    feat: 사용자 분석 조회 함수 연결

--- src/users/application/users.service.ts
+++ src/users/application/users.service.ts
@@ -3,9 +3,15 @@ import { Inject, Injectable } from '@nestjs/common';
 import { IUsersRepository, USER_REPOSITORY } from '../domain/users.repository.interface';
 import { User } from '../domain/users.entity';
 
+import { UserMistakeAnalysis } from '@games/domain/user-mistake-analysis.entity';
+import { GamesService } from '@games/application/games.service';
+
 @Injectable()
 export class UsersService {
-  constructor(@Inject(USER_REPOSITORY) private readonly usersRepository: IUsersRepository) {}
+  constructor(
+    @Inject(USER_REPOSITORY) private readonly usersRepository: IUsersRepository,
+    private readonly gamesService: GamesService,
+  ) {}
 
   async getRanksByPageAndSize(
     page: number,
@@ -16,5 +22,9 @@ export class UsersService {
       this.usersRepository.findAllOrderByScoreDesc(page, size, tierId),
       this.usersRepository.countAll(tierId),
     ]);
+  }
+
+  async getUserAnalysis(userId: bigint): Promise<UserMistakeAnalysis> {
+    return this.gamesService.getUserMistakeAnalysis(userId);
   }
 }
