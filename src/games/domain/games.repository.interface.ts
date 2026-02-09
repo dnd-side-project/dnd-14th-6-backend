76e200c3f9f2db7267e337b383174a65cdaffe55
jokj624 <jokj624@gmail.com>
2026-02-10 01:33:36 +0900

    feat: games repository에 명령어, 카테고리 분석 함수 interface 추가

--- src/games/domain/games.repository.interface.ts
+++ src/games/domain/games.repository.interface.ts
@@ -1,4 +1,5 @@
 import { GameCategory } from './game-categories.entity';
+import { FrequentWrongCommand, FrequentWrongCategory } from './user-mistake-analysis.entity';
 
 export const GAME_REPOSITORY = Symbol('IGameRepository');
 
@@ -7,4 +8,14 @@ export interface IGameRepository {
    * @description 모든 카테고리 목록을 조회
    */
   getCategories(): Promise<GameCategory[]>;
+
+  /**
+   * @description 사용자가 자주 틀린 명령어 Top 5 조회 (서브 카테고리 별)
+   */
+  getFrequentWrongCommands(userId: bigint): Promise<FrequentWrongCommand[]>;
+
+  /**
+   * @description 사용자가 자주 틀린 카테고리 조회 (오답 비율 포함)
+   */
+  getFrequentWrongCategories(userId: bigint): Promise<FrequentWrongCategory[]>;
 }
