0df7cf375235a76ec1c362dfdfb66b52e6b8037d
jokj624 <jokj624@gmail.com>
2026-02-10 01:37:49 +0900

    feat: analysis API 컨트롤러 함수

--- src/users/presentation/users.controller.ts
+++ src/users/presentation/users.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { UsersService } from '../application/users.service';
 import { GetRanksQueryDto, GetRanksResponseDto } from './dto/get-ranks.dto';
+import { GetUserAnalysisParamDto, GetUserAnalysisResponseDto } from './dto/get-user-analysis.dto';
 import { ApiGetRanks } from './decorators/get-ranks-swagger.decorator';
+import { ApiGetUserAnalysis } from './dto/decorators/get-user-analysis-swagger.decorator';
 
 @ApiTags('Users')
 @Controller('users')
@@ -18,5 +20,15 @@ export class UsersController {
     const [users, totalItems] = await this.usersService.getRanksByPageAndSize(page, size, tierId);
 
     return GetRanksResponseDto.from(users, totalItems, page, size);
+  }
+
+  @Get('/:userId/analysis')
+  @ApiGetUserAnalysis()
+  async getUserAnalysis(
+    @Param() param: GetUserAnalysisParamDto,
+  ): Promise<GetUserAnalysisResponseDto> {
+    const userAnalysis = await this.usersService.getUserAnalysis(param.userId);
+
+    return GetUserAnalysisResponseDto.from(userAnalysis);
   }
 }
