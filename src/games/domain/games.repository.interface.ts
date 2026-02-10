def32bdcd5e69736e0bd4c5dfb60d79ac348a7a7
eunkang-choi <dmsrkd1216@gmail.com>
2026-02-10 14:52:26 +0900

    feat: 게임문제 출제 - repository interface 추상메서드 추가

--- src/games/domain/games.repository.interface.ts
+++ src/games/domain/games.repository.interface.ts
@@ -1,10 +1,24 @@
 import { GameCategory } from './game-categories.entity';
+import { GameProblem } from './game-problem.entity';
+import { GameDifficultyMode } from './game.business-rules';
 
 export const GAME_REPOSITORY = Symbol('IGameRepository');
 
 export interface IGameRepository {
   /**
+   * @description 특정 카테고리와 난이도에 맞는 문제 MAX_PROBLEMS_PER_GAME개 랜덤으로 출제
+   */
+  getProblems(categoryId: number, difficulty: GameDifficultyMode): Promise<GameProblem[]>;
+  /**
+   * @description 특정 카테고리의 난이도가 랜덤으로 문제 MAX_PROBLEMS_PER_GAME개 랜덤으로 출제
+   */
+  getRandomProblems(categoryId: number): Promise<GameProblem[]>;
+  /**
    * @description 모든 카테고리 목록을 조회
    */
   getCategories(): Promise<GameCategory[]>;
+  /**
+   * @description categoryId로 카테고리 존재여부 확인
+   */
+  categoryExists(categoryId: number): Promise<boolean>;
 }
