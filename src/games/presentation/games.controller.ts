26b466ccaf58d1342ac5554745d43692fe52a9e8
eunkang-choi <dmsrkd1216@gmail.com>
2026-02-10 20:40:38 +0900

    feat: 게임스트림 컨트롤러 메서드 추가

--- src/games/presentation/games.controller.ts
+++ src/games/presentation/games.controller.ts
@@ -1,18 +1,73 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, MessageEvent, Query, Req, Res } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { GamesService } from '../application/games.service';
 import { GetGameOptionsResponseDto } from './dto/get-game-options.dto';
 import { ApiGetGameOptions } from './decorators/get-game-options-swagger.decorator';
+import { ApiGameStream } from './decorators/game-stream-swagger.decorator';
+import { Request, Response } from 'express';
+import { Subject } from 'rxjs';
+import { GameStreamService } from '../application/game-stream.service';
+import { GameStreamQueryDto } from './dto/game-stream-query.dto';
 
 @ApiTags('Games')
 @Controller('games')
 export class GamesController {
-  constructor(private readonly gameService: GamesService) {}
+  constructor(
+    private readonly gameService: GamesService,
+    private readonly gameStreamService: GameStreamService,
+  ) {}
 
   @Get('options')
   @ApiGetGameOptions()
   async getGameOptions(): Promise<GetGameOptionsResponseDto> {
     const gameOptions = await this.gameService.getGameOptions();
     return GetGameOptionsResponseDto.from(gameOptions);
+  }
+
+  @Get('stream')
+  @ApiGameStream()
+  async gameStream(
+    @Query() query: GameStreamQueryDto,
+    @Req() request: Request,
+    @Res() response: Response,
+  ): Promise<void> {
+    await this.gameStreamService.validateGameStreamParams(query.categoryId);
+
+    response.setHeader('Content-Type', 'text/event-stream');
+    response.setHeader('Cache-Control', 'no-cache');
+    response.setHeader('Connection', 'keep-alive');
+    response.flushHeaders();
+
+    const disconnectSignal$ = new Subject<void>();
+    request.on('close', () => {
+      disconnectSignal$.next();
+      disconnectSignal$.complete();
+    });
+
+    const stream$ = this.gameStreamService.createGameStream(
+      query.categoryId,
+      query.difficultyMode,
+      disconnectSignal$,
+    );
+
+    stream$.subscribe({
+      next: (event: MessageEvent) => {
+        const sseData = JSON.stringify({
+          statusCode: 200,
+          success: true,
+          data: event.data,
+        });
+        if (event.type) {
+          response.write(`event: ${event.type}\n`);
+        }
+        response.write(`data: ${sseData}\n\n`);
+      },
+      complete: () => {
+        response.end();
+      },
+      error: () => {
+        response.end();
+      },
+    });
   }
 }
