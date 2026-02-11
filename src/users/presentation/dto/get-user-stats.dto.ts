55c994855bcbaf10ab393af23a0c04f9fd2bdfae
jokj624 <jokj624@gmail.com>
2026-02-11 23:25:53 +0900

    fix: userId empty validation 에러 메시지 추가

--- src/users/presentation/dto/get-user-stats.dto.ts
+++ src/users/presentation/dto/get-user-stats.dto.ts
@@ -10,7 +10,7 @@ export class GetUserStatsParamDto {
     description: '유저 ID',
     example: '1',
   })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'userId는 필수값입니다.' })
   @Transform(({ value }) => {
     try {
       return BigInt(value as string);
