d8de0248e2883d2c3b4c9f2d6f0497a75503f0cd
eunkang-choi <dmsrkd1216@gmail.com>
2026-02-12 02:59:23 +0900

    refactor: 필드타입 ProblemDifficulty로 변경 & dead-code 제거

--- src/games/infrastructure/types/problem-raw-row.ts
+++ src/games/infrastructure/types/problem-raw-row.ts
@@ -1,8 +1,10 @@
+import { ProblemDifficulty } from '@games/domain/game.business-rules';
+
 export interface ProblemRawRow {
   id: bigint;
   title: string;
   text: string;
   answer: string;
-  difficulty: string;
+  difficulty: ProblemDifficulty;
   subCategoryName: string;
 }
