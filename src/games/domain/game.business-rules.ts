f57ccd71f987794af2b08adead1ed4c74a401f5a
jokj624 <jokj624@gmail.com>
2026-02-12 23:56:09 +0900

    feat: game history 조회 정렬 filter business rule 추가

--- src/games/domain/game.business-rules.ts
+++ src/games/domain/game.business-rules.ts
@@ -9,3 +9,20 @@ export enum GameDifficultyMode {
 }
 
 export const DIFFICULTY_MODES: GameDifficultyMode[] = Object.values(GameDifficultyMode);
+
+/**
+ * 게임 세션 히스토리 정렬 기준
+ */
+export enum GameSessionSortBy {
+  PlayedAt = 'playedAt',
+  Score = 'score',
+  CorrectProblemCount = 'correctProblemCount',
+}
+
+/**
+ * 정렬 방향
+ */
+export enum SortOrder {
+  Asc = 'asc',
+  Desc = 'desc',
+}
