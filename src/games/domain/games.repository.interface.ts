5a8d1b738956e7387f5dbc6b249f9b4cdf7f4d66
jokj624 <jokj624@gmail.com>
2026-02-12 23:54:16 +0900

    feat: getSessionHistoryByFilter interface 선언

--- src/games/domain/games.repository.interface.ts
+++ src/games/domain/games.repository.interface.ts
@@ -1,4 +1,6 @@
 import { GameCategory } from './game-categories.entity';
+import { GameSessionHistoryFilterEntity } from './game-session-history-filter.entity';
+import { GameSessionHistoryList } from './game-session-history.entity';
 
 export const GAME_REPOSITORY = Symbol('IGameRepository');
 
@@ -7,4 +9,11 @@ export interface IGameRepository {
    * @description 모든 카테고리 목록을 조회
    */
   getCategories(): Promise<GameCategory[]>;
+
+  /**
+   * @description 필터 기반 사용자 게임 세션 히스토리 목록 조회
+   */
+  getSessionHistoryByFilter(
+    filter: GameSessionHistoryFilterEntity,
+  ): Promise<GameSessionHistoryList>;
 }
