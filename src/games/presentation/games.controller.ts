db06c6848db27d702a5bd0434a51624cd2d5bd9c
jokj624 <jokj624@gmail.com>
2026-02-12 23:57:47 +0900

    feat: game history 조회 API controller 함수 추가

--- src/games/presentation/games.controller.ts
+++ src/games/presentation/games.controller.ts
@@ -1,18 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { GamesService } from '../application/games.service';
+import { GameSessionService } from '../application/game-session.service';
 import { GetGameOptionsResponseDto } from './dto/get-game-options.dto';
 import { ApiGetGameOptions } from './decorators/get-game-options-swagger.decorator';
+import { ApiGetGameHistories } from './decorators/get-game-histories-swagger.decorator';
+import {
+  GetGameHistoriesQueryDto,
+  GetGameHistoriesResponseDto,
+} from './dto/get-game-histories.dto';
 
 @ApiTags('Games')
 @Controller('games')
 export class GamesController {
-  constructor(private readonly gameService: GamesService) {}
+  constructor(
+    private readonly gameService: GamesService,
+    private readonly gameSessionService: GameSessionService,
+  ) {}
 
   @Get('options')
   @ApiGetGameOptions()
   async getGameOptions(): Promise<GetGameOptionsResponseDto> {
     const gameOptions = await this.gameService.getGameOptions();
     return GetGameOptionsResponseDto.from(gameOptions);
+  }
+
+  @Get('sessions')
+  @ApiGetGameHistories()
+  async getGameHistories(
+    @Query() query: GetGameHistoriesQueryDto,
+  ): Promise<GetGameHistoriesResponseDto> {
+    const gameHistories = await this.gameSessionService.getSessionHistories(query);
+
+    return GetGameHistoriesResponseDto.from(gameHistories, query.page, query.size);
   }
 }
