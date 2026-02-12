2b61d91beb6d78d491cb3fd336470ffc9e2b0c9e
jokj624 <jokj624@gmail.com>
2026-02-12 23:58:44 +0900

    feat: game session service provider 등록

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '@prisma/prisma.module';
 import { GamesService } from './application/games.service';
+import { GameSessionService } from './application/game-session.service';
 import { GamesController } from './presentation/games.controller';
 import { GAME_REPOSITORY } from './domain/games.repository.interface';
 import { GameRepositoryImpl } from './infrastructure/games.repository';
-import { PrismaModule } from '@prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   providers: [
     GamesService,
+    GameSessionService,
     {
       provide: GAME_REPOSITORY,
       useClass: GameRepositoryImpl,
