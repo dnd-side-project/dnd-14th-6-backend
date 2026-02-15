f5a31e428fe5051f93fb4b79afadd46a757930fe
eunkang-choi <dmsrkd1216@gmail.com>
2026-02-15 18:09:47 +0900

    remove: 불필요코드 제거

--- src/games/presentation/decorators/get-game-options-swagger.decorator.ts
+++ src/games/presentation/decorators/get-game-options-swagger.decorator.ts
@@ -1,8 +1,8 @@
 import { applyDecorators } from '@nestjs/common';
 import { ApiExtraModels, ApiOkResponse, ApiOperation, getSchemaPath } from '@nestjs/swagger';
 
-import { createSwaggerServerErrors } from '@/common/utils/swagger-error-response.util';
 import { ApiResponseDto } from '@common/dto/api-response.dto';
+import { createSwaggerServerErrors } from '@common/utils/swagger-error-response.util';
 
 import { CategoryDto, GetGameOptionsResponseDto } from '../dto/get-game-options.dto';
 
@@ -23,18 +23,6 @@ export function ApiGetGameOptions() {
                 },
               },
             ],
-          },
-          example: {
-            statusCode: 200,
-            success: true,
-            data: {
-              categories: [
-                { id: 1, name: 'Git', iconUrl: 'https://cdn.orvit.net/categories/git.png' },
-                { id: 2, name: 'Linux', iconUrl: 'https://cdn.orvit.net/categories/linux.png' },
-                { id: 3, name: 'Docker', iconUrl: 'https://cdn.orvit.net/categories/docker.png' },
-              ],
-              difficultyModes: ['Easy', 'Normal', 'Hard', 'Random'],
-            },
           },
         },
       },
