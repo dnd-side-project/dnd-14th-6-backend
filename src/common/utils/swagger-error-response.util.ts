562ea7b273c0f9dfeacca181c024ad0db0b4950b
jokj624 <jokj624@gmail.com>
2026-02-16 02:22:32 +0900

    fix: swagger 400 명세 상태 필드명 수정

--- src/common/utils/swagger-error-response.util.ts
+++ src/common/utils/swagger-error-response.util.ts
@@ -18,7 +18,7 @@ export function createSwaggerBadRequest(examples: ErrorExample[]) {
   examples.forEach((data) => {
     exampleObject[data.description] = {
       value: {
-        status: 400,
+        statusCode: 400,
         success: false,
         message: data.message,
       },
