bde257c8a1020bcacb071202cad8a346491c8e8a
eunkang-choi <dmsrkd1216@gmail.com>
2026-02-15 18:02:35 +0900

    refactor: 게임옵션 선택 api swagger 리팩터링

--- src/games/presentation/decorators/get-game-options-swagger.decorator.ts
+++ src/games/presentation/decorators/get-game-options-swagger.decorator.ts
@@ -1,36 +1,44 @@
 import { applyDecorators } from '@nestjs/common';
-import {
-  ApiExtraModels,
-  ApiOkResponse,
-  ApiOperation,
-  ApiResponse,
-  getSchemaPath,
-} from '@nestjs/swagger';
+import { ApiExtraModels, ApiOkResponse, ApiOperation, getSchemaPath } from '@nestjs/swagger';
 
+import { createSwaggerServerErrors } from '@/common/utils/swagger-error-response.util';
 import { ApiResponseDto } from '@common/dto/api-response.dto';
-import { GetGameOptionsResponseDto } from '../dto/get-game-options.dto';
+
+import { CategoryDto, GetGameOptionsResponseDto } from '../dto/get-game-options.dto';
 
 export function ApiGetGameOptions() {
   return applyDecorators(
     ApiOperation({ summary: '게임 옵션 조회 (카테고리, 난이도)' }),
-    ApiExtraModels(ApiResponseDto, GetGameOptionsResponseDto),
+    ApiExtraModels(ApiResponseDto, GetGameOptionsResponseDto, CategoryDto),
     ApiOkResponse({
       description: '게임 옵션 조회 성공',
-      schema: {
-        type: 'object',
-        $ref: getSchemaPath(ApiResponseDto),
-        properties: {
-          data: { $ref: getSchemaPath(GetGameOptionsResponseDto) },
+      content: {
+        'application/json': {
+          schema: {
+            allOf: [
+              { $ref: getSchemaPath(ApiResponseDto) },
+              {
+                properties: {
+                  data: { $ref: getSchemaPath(GetGameOptionsResponseDto) },
+                },
+              },
+            ],
+          },
+          example: {
+            statusCode: 200,
+            success: true,
+            data: {
+              categories: [
+                { id: 1, name: 'Git', iconUrl: 'https://cdn.orvit.net/categories/git.png' },
+                { id: 2, name: 'Linux', iconUrl: 'https://cdn.orvit.net/categories/linux.png' },
+                { id: 3, name: 'Docker', iconUrl: 'https://cdn.orvit.net/categories/docker.png' },
+              ],
+              difficultyModes: ['Easy', 'Normal', 'Hard', 'Random'],
+            },
+          },
         },
       },
     }),
-    ApiResponse({
-      status: 500,
-      description: 'Internal Server Error',
-    }),
-    ApiResponse({
-      status: 503,
-      description: 'Service Unavailable',
-    }),
+    ...createSwaggerServerErrors(),
   );
 }
