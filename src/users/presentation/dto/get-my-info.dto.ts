dc12c9b4f43ec68aae6af7c61d015ea1e96a85f4
jokj624 <jokj624@gmail.com>
2026-02-17 03:35:20 +0900

    feat: 내 정보 조회 API response dto

---
+++ src/users/presentation/dto/get-my-info.dto.ts
@@ -0,0 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+import { plainToInstance } from 'class-transformer';
+
+import { User } from '@users/domain/users.entity';
+
+export class GetMyInfoResponseDto {
+  @ApiProperty({ description: '유저 ID', example: '121' })
+  id: string;
+
+  @ApiProperty({ description: '유지 닉네임', example: 'John' })
+  nickname: string;
+
+  @ApiProperty({
+    description: '유저 프로필 사진 (없을 경우 default image)',
+    example: 'https://example.com/profile.png',
+  })
+  profileImage: string;
+
+  static from(user: User) {
+    return plainToInstance(GetMyInfoResponseDto, {
+      id: user.id.toString(),
+      nickname: user.nickname,
+      profileImage: user.profileImage,
+    });
+  }
+}
