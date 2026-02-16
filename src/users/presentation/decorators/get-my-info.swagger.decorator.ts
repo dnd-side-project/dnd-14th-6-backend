14f03953dd90b3dd7c5f65fbf08e938c5fae0f5d
jokj624 <jokj624@gmail.com>
2026-02-17 03:40:16 +0900

    feat: 내 정보 조회 API swagger decorator

---
+++ src/users/presentation/decorators/get-my-info.swagger.decorator.ts
@@ -0,0 +1,32 @@
+import { applyDecorators } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiExtraModels,
+  ApiOkResponse,
+  ApiOperation,
+  getSchemaPath,
+} from '@nestjs/swagger';
+
+import { createSwaggerAuthErrors } from '@/common/utils/swagger-error-response.util';
+import { ApiResponseDto } from '@common/dto/api-response.dto';
+
+import { GetMyInfoResponseDto } from '../dto/get-my-info.dto';
+
+export function ApiGetMyInfo() {
+  return applyDecorators(
+    ApiBearerAuth(),
+    ApiOperation({ summary: '내 정보 조회' }),
+    ApiExtraModels(ApiResponseDto, GetMyInfoResponseDto),
+    ApiOkResponse({
+      description: '내 정보 조회 성공',
+      schema: {
+        type: 'object',
+        $ref: getSchemaPath(ApiResponseDto),
+        properties: {
+          data: { $ref: getSchemaPath(GetMyInfoResponseDto) },
+        },
+      },
+    }),
+    ...createSwaggerAuthErrors(),
+  );
+}
