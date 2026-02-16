0c83d31ccd5e9bea0897b58777a51d7d320608d0
jokj624 <jokj624@gmail.com>
2026-02-17 01:22:44 +0900

    feat: auth module에 refresh token guard, strategy 등록

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,18 +2,20 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+
 import { GamesModule } from '@games/games.module';
 import { TiersModule } from '@tiers/tiers.module';
 import { UsersModule } from '@users/users.module';
 
-import { AuthController } from './presentation/auth.controller';
-import { GoogleAuthGuard } from './presentation/guards/google-auth.guard';
-import { GithubAuthGuard } from './presentation/guards/github-auth.guard';
-import { GoogleStrategy } from './presentation/strategies/google.strategy';
-import { GithubStrategy } from './presentation/strategies/github.strategy';
-
 import { AuthFacade } from './application/auth.facade';
 import { AuthService } from './application/auth.service';
+import { AuthController } from './presentation/auth.controller';
+import { GithubAuthGuard } from './presentation/guards/github-auth.guard';
+import { GoogleAuthGuard } from './presentation/guards/google-auth.guard';
+import { JwtRefreshAuthGuard } from './presentation/guards/jwt-refresh-auth.guard';
+import { GithubStrategy } from './presentation/strategies/github.strategy';
+import { GoogleStrategy } from './presentation/strategies/google.strategy';
+import { JwtRefreshStrategy } from './presentation/strategies/jwt-refresh.strategy';
 
 @Module({
   imports: [
@@ -35,6 +37,8 @@ import { AuthService } from './application/auth.service';
     GoogleAuthGuard,
     GithubStrategy,
     GithubAuthGuard,
+    JwtRefreshStrategy,
+    JwtRefreshAuthGuard,
   ],
   controllers: [AuthController],
 })
