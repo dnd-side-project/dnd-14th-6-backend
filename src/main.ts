b3e9b74ffdfc06c3c6cc83db13121d57d9d30e04
eunkang-choi <dmsrkd1216@gmail.com>
2026-02-17 15:10:22 +0900

    fix: 쿠키 기반 인증에서 필요한 옵션 추가

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     origin: ['http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
 
   setupSwagger(app);
