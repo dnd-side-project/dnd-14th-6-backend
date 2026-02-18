3abadbdaae1a41b49ab1987be6daf7c66d715540
jokj624 <jokj624@gmail.com>
2026-02-18 14:39:59 +0900

    fix: import path alias 수정

--- src/auth/presentation/strategies/jwt-refresh.strategy.ts
+++ src/auth/presentation/strategies/jwt-refresh.strategy.ts
@@ -2,10 +2,9 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 
+import { AuthFacade } from '@auth/application/auth.facade';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-
-import { AuthFacade } from '@/auth/application/auth.facade';
 
 function getRefreshToken(request: Request): string | null {
   const body = request.body as { refreshToken?: string };
