1b789b70ff5de016b6bbbd43b3ca04c400dfa174
eunkang-choi <dmsrkd1216@gmail.com>
2026-02-19 20:11:10 +0900

    feat: 게임세션저장 facade 레이어로직 작성

---
+++ src/games/application/game.facade.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+
+import { UsersService } from '@users/application/users.service';
+
+import {
+  CreateGameSessionFacadeRequestDto,
+  CreatGameSessionFacadeResponseDto,
+} from './facade-dto/create-game-session.service-dto';
+import { GameSessionService } from './game-session.service';
+import { GamesService } from './games.service';
+
+@Injectable()
+export class GameFacade {
+  constructor(
+    private readonly gameService: GamesService,
+    private readonly gameSessionService: GameSessionService,
+    private readonly userService: UsersService,
+  ) {}
+
+  /*
+   * @description 게임 세션 저장
+   */
+  async saveGameSession(
+    facadeDto: CreateGameSessionFacadeRequestDto,
+  ): Promise<CreatGameSessionFacadeResponseDto> {
+    const gameSessionId = await this.gameSessionService.createGameSession({
+      categoryId: facadeDto.categoryId,
+      difficultyMode: facadeDto.difficultyMode,
+      score: facadeDto.score,
+      clientAnswers: facadeDto.clientAnswers,
+      userId: facadeDto.userId,
+    });
+
+    let totalScore: bigint | undefined = undefined;
+    if (facadeDto.userId) {
+      totalScore = await this.gameSessionService.getTotalScoreByUserId(facadeDto.userId);
+      await this.userService.updateTotalScore(facadeDto.userId, totalScore);
+    }
+
+    return {
+      gameSessionId,
+      totalScore,
+    };
+  }
+}
