3c1ae1b0451a88a13c41800fafee2cbb515083c0
eunkang-choi <dmsrkd1216@gmail.com>
2026-02-22 06:39:37 +0900

    refactor: saveGameSession facade 내부로직 리팩터링

--- src/games/application/game.facade.ts
+++ src/games/application/game.facade.ts
@@ -24,18 +24,29 @@ export class GameFacade {
   async saveGameSession(
     facadeDto: SaveGameSessionFacadeRequestDto,
   ): Promise<SaveGameSessionFacadeResponseDto> {
+    // 클라이언트값의 facadeDto.score가 검증된 점수인지 확인.
+    const serverScore = await this.gameSessionService.validateAndCalculateScore(
+      facadeDto.categoryId,
+      facadeDto.clientAnswers,
+    );
+
+    // 게임세션 저장
     const gameSessionId = await this.gameSessionService.createGameSession({
       categoryId: facadeDto.categoryId,
       difficultyMode: facadeDto.difficultyMode,
-      score: facadeDto.score,
+      score: serverScore,
       clientAnswers: facadeDto.clientAnswers,
       userId: facadeDto.userId,
     });
 
     let totalScore: bigint | undefined = undefined;
     if (facadeDto.userId) {
-      totalScore = await this.gameSessionService.getTotalScoreByUserId(facadeDto.userId);
-      await this.userService.updateTotalScore(facadeDto.userId, totalScore);
+      // (회원인경우) 유저의 totalScore에 서버 점수를 증분 업데이트
+      totalScore = await this.userService.incrementTotalScore(
+        facadeDto.userId,
+        BigInt(serverScore),
+      );
+      // FIXME: 게임점수 저장후, 유저의 totalScore가 다음티어로 승급이 되는지 확인. (현재티어보다 한단계높은 티어의 minScore이상인지 확인)
     }
 
     return {
