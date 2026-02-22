475c46ae6497a6d77c8a1d242caefbe657208b4e
jokj624 <jokj624@gmail.com>
2026-02-23 01:26:40 +0900

    refactor: game service에 있던 통계, 분석 관련 로직을 별도 서비스로 분리

--- src/games/application/games.service.ts
+++ src/games/application/games.service.ts
@@ -2,7 +2,6 @@ import { Inject, Injectable } from '@nestjs/common';
 
 import { GameOptions } from '../domain/game-options.entity';
 import { GAME_REPOSITORY, IGameRepository } from '../domain/games.repository.interface';
-import { UserMistakeAnalysis } from '../domain/user-mistake-analysis.entity';
 
 @Injectable()
 export class GamesService {
@@ -15,22 +14,5 @@ export class GamesService {
   async getGameOptions(): Promise<GameOptions> {
     const categories = await this.gameRepository.getCategories();
     return GameOptions.from(categories);
-  }
-
-  /*
-   * @description 사용자의 명령어, 카테고리 실수 분석 조회
-   * - 자주 틀린 명령어 Top 5 (서브 카테고리 별)
-   * - 자주 틀린 카테고리 (오답 비율 포함)
-   */
-  async getUserMistakeAnalysis(userId: bigint): Promise<UserMistakeAnalysis> {
-    const [frequentWrongCommands, frequentWrongCategories] = await Promise.all([
-      this.gameRepository.getFrequentWrongCommands(userId),
-      this.gameRepository.getFrequentWrongCategories(userId),
-    ]);
-
-    return UserMistakeAnalysis.from({
-      frequentWrongCommands,
-      frequentWrongCategories,
-    });
   }
 }
