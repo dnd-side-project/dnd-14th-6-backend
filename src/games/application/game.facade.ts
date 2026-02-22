3c26407a9485dbb2ec7270b5559d65c1ebe0dd68
eunkang-choi <dmsrkd1216@gmail.com>
2026-02-22 13:27:23 +0900

    refactor: 유저용 totalScore, 티어승급 로직은 별도의 private함수로 리팩터링

--- src/games/application/game.facade.ts
+++ src/games/application/game.facade.ts
@@ -21,18 +21,21 @@ export class GameFacade {
 
   /*
    * @description 게임 세션 저장
+   *
+   * - 클라이언트의 입력데이터가 올바른 형태인지 유효성 검증하고 서버에서 게임점수를 한번더 재검증합니다. 재검증된 게임점수를 serverScore라 합니다.
+   * - 검증된 점수 serverScore 를 게임세션에 테이블에 저장합니다.
+   * - 유저의 경우에는 유저의 totalScore에  serverScore 을 누적합니다.
+   * - totalScore 업데이트로 티어등급이 유저의 현재 티어등급과 다르다면, 승급되었으므로 totalScore 기반의 티어등급으로 업데이트합니다.
    */
   @Transactional()
   async saveGameSession(
     facadeDto: SaveGameSessionFacadeRequestDto,
   ): Promise<SaveGameSessionFacadeResponseDto> {
-    // 클라이언트값의 facadeDto.score가 검증된 점수인지 확인.
     const serverScore = await this.gameSessionService.validateAndCalculateScore(
       facadeDto.categoryId,
       facadeDto.clientAnswers,
     );
 
-    // 게임세션 저장
     const gameSessionId = await this.gameSessionService.createGameSession({
       categoryId: facadeDto.categoryId,
       difficultyMode: facadeDto.difficultyMode,
@@ -43,22 +46,30 @@ export class GameFacade {
 
     let totalScore: bigint | undefined = undefined;
     if (facadeDto.userId) {
-      // (회원인경우) 유저의 totalScore에 서버 점수를 증분 업데이트
-      totalScore = await this.userService.incrementTotalScore(
-        facadeDto.userId,
-        BigInt(serverScore),
-      );
-
-      // 티어 승급 체크 & 업데이트
-      const newTier = await this.tiersService.findTierByScore(totalScore);
-      if (newTier) {
-        await this.userService.updateTierId(facadeDto.userId, newTier.id);
-      }
+      totalScore = await this.updateTotalScoreAndTierForUser(facadeDto.userId, serverScore);
     }
 
     return {
       gameSessionId,
       totalScore,
     };
+  }
+
+  /*
+   * @description (유저용) 게임결과 저장이후 유저의 totalScore와 totalScore 업데이트로 인해 유저의 티어 업데이트
+   */
+  private async updateTotalScoreAndTierForUser(
+    userId: bigint,
+    serverScore: number,
+  ): Promise<bigint> {
+    const totalScore = await this.userService.incrementTotalScore(userId, BigInt(serverScore));
+
+    const currentUser = await this.userService.findById(userId);
+    const newTier = await this.tiersService.findTierByScore(totalScore);
+    if (currentUser.tierId !== newTier.id) {
+      await this.userService.updateTierId(userId, newTier.id);
+    }
+
+    return totalScore;
   }
 }
